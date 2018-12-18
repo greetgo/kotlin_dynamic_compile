aeab547f749c005e2069b19fc4b1a395c4812b2f
ekolpakov <ekolpakov@greet-go.com>
2018-12-18 14:14:30 +0600

    Killed client class

--- src/kz/pompei/dynamic_compile/Client.kt
+++
@@ -1,6 +0,0 @@
-package kz.pompei.dynamic_compile
-
-data class Client(
-  val id: String,
-  var name: String? = null
-)
--- src/kz/pompei/dynamic_compile/ClientService.kt
+++
@@ -1,9 +0,0 @@
-package kz.pompei.dynamic_compile
-
-interface ClientService {
-
-  fun loadClient(id: String): Client
-
-  fun saveClient(client: Client)
-
-}
